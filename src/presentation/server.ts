18ef6abfcd57f1e64c3c977fbd59aa5550a81532
franklin rodriguez <franklinserif@gmail.com>
2024-09-10 16:49:18 -0400

    feat(server): implemente fileSytemLogRepository

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,12 +1,19 @@
 import { CronService } from '@presentation/cron/cron-service'
 import { CheckService } from '@domain/use-case/checks/check-service'
+import { LogRepositoryImplementation } from '@infrastructure/repositories/log.repository.impl'
+import { FileSystemDatasource } from '@infrastructure/datasource/file-system.datasource'
+
+const fileSystemLogRepository = new LogRepositoryImplementation(
+    new FileSystemDatasource()
+)
 
 export class Server {
     public static start() {
         console.log('server started')
-        const url = 'https://google.com'
+        const url = 'http://localhost:3000'
         CronService.createJob('*/5 * * * * *', () => {
             new CheckService(
+                fileSystemLogRepository,
                 () => console.log(`success ${url}`),
                 (error) => console.log(error)
             ).execute(url)
