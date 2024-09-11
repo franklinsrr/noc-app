3098c98c065d4c2743e250c90c6d34405e758eb4
franklin rodriguez <franklinserif@gmail.com>
2024-09-11 11:47:41 -0400

    feat: services

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,33 +1,28 @@
 import { CronService } from '@presentation/cron/cron-service'
-import { EmailService } from '@presentation/email/email.service'
-import { CheckService } from '@domain/use-case/checks/check-service'
 import { LogRepositoryImplementation } from '@infrastructure/repositories/log.repository.impl'
 import { FileSystemDatasource } from '@infrastructure/datasource/file-system.datasource'
-import { SendEmailLogs } from '@domain/use-case/email/send-email-logs'
+import { PostgresLogDatasource } from '@infrastructure/datasource/postgres-log.datasource'
+import { MongoLogDatasource } from '@infrastructure/datasource/mongo-log.datasource'
+import { CheckServiceMultiple } from '@domain/use-case/checks/check-service-multiple'
 
-const fileSystemLogRepository = new LogRepositoryImplementation(
-    new FileSystemDatasource()
+const fsRepository = new LogRepositoryImplementation(new FileSystemDatasource())
+
+const mongoRepository = new LogRepositoryImplementation(
+    new MongoLogDatasource()
 )
 
-const emailService = new EmailService()
+const postgresRepository = new LogRepositoryImplementation(
+    new PostgresLogDatasource()
+)
 
 export class Server {
     public static start() {
         console.log('server started')
 
-        new SendEmailLogs(emailService, fileSystemLogRepository).execute([
-            'franklingra@outlook.com',
-            'franklinserif@gmail.com',
-        ])
-        /*  emailService.sendEmailWidthFileSystemLogs([
-            'franklingra@outlook.com',
-            'franklinserif@gmail.com',
-        ])  */
-
-        const url = 'https://google.com'
-        CronService.createJob('*/5 * * * * *', () => {
-            new CheckService(
-                fileSystemLogRepository,
+        const url = 'https://faceqwdqwdbook.com'
+        CronService.createJob('*/2 * * * * *', () => {
+            new CheckServiceMultiple(
+                [fsRepository, mongoRepository, postgresRepository],
                 () => console.log(`success ${url}`),
                 (error) => console.log(error)
             ).execute(url)
