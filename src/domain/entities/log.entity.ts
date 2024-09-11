79d12bdbe53c72076967841149b3e0c8260ef61e
franklin rodriguez <franklinserif@gmail.com>
2024-09-10 20:58:41 -0400

    refactor(log.entity.ts): improve implementation

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,27 +4,39 @@ export enum LogSeverityLevel {
     hight = 'hight',
 }
 
+export interface LogEntityOptions {
+    level: LogSeverityLevel
+    message: string
+    createdAt?: Date
+    origin: string
+}
+
 export class LogEntity {
     public level: LogSeverityLevel
     public message: string
     public createdAt: Date
+    public origin: string
 
-    constructor(message: string, level: LogSeverityLevel) {
+    constructor({ message, level, origin }: LogEntityOptions) {
         this.message = message
         this.level = level
+        this.origin = origin
         this.createdAt = new Date()
     }
 
     static fromJson(json: string): LogEntity {
-        const { message, level, createdAt } = JSON.parse(json)
+        const { message, level, createdAt, origin } = JSON.parse(json)
 
         if (!message) throw new Error('Message is required')
         if (!level) throw new Error('Message is required')
         if (!createdAt) throw new Error('Message is required')
 
-        const log = new LogEntity(message, level)
-        log.createdAt = new Date(createdAt)
-
+        const log = new LogEntity({
+            message,
+            level,
+            createdAt,
+            origin,
+        })
         return log
     }
 }
