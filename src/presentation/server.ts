04f7fc421c84f2c9100e7fb42372350bf27eccb8
franklin rodriguez <franklinserif@gmail.com>
2024-09-10 21:00:00 -0400

    feat(server): implement new use cases

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,16 +1,30 @@
 import { CronService } from '@presentation/cron/cron-service'
+import { EmailService } from '@presentation/email/email.service'
 import { CheckService } from '@domain/use-case/checks/check-service'
 import { LogRepositoryImplementation } from '@infrastructure/repositories/log.repository.impl'
 import { FileSystemDatasource } from '@infrastructure/datasource/file-system.datasource'
+import { SendEmailLogs } from '@domain/use-case/email/send-email-logs'
 
 const fileSystemLogRepository = new LogRepositoryImplementation(
     new FileSystemDatasource()
 )
 
+const emailService = new EmailService()
+
 export class Server {
     public static start() {
         console.log('server started')
-        const url = 'http://localhost:3000'
+
+        new SendEmailLogs(emailService, fileSystemLogRepository).execute([
+            'franklingra@outlook.com',
+            'franklinserif@gmail.com',
+        ])
+        /*  emailService.sendEmailWidthFileSystemLogs([
+            'franklingra@outlook.com',
+            'franklinserif@gmail.com',
+        ])  */
+
+        const url = 'https://google.com'
         CronService.createJob('*/5 * * * * *', () => {
             new CheckService(
                 fileSystemLogRepository,
