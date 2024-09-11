1d30df682b019714d8085ecd4dd9d32e5f4b244f
franklin rodriguez <franklinserif@gmail.com>
2024-09-11 11:47:32 -0400

    feat(server): mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import { Server } from '@presentation/server'
+import { MongoDatase } from '@data/mongo/init'
+import { envs } from '@config/plugins/envs.plugins'
 
-Server.start()
+const start = async () => {
+    MongoDatase.connect({
+        mongoURL: envs.MONGO_URL,
+        dbName: envs.MONGO_DB_NAME,
+    })
+
+    Server.start()
+}
+
+start()
